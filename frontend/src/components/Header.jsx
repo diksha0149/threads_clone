4b076bf61ad42444039e75fdeaa7333ad535eb45
doriroz <doriroz@gmail.com>
2024-08-10 21:49:35 +0300

    update login interface

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,11 @@
-import { Button, Flex, Image, Link, useColorMode } from "@chakra-ui/react";
+import {
+  Button,
+  Spacer,
+  Flex,
+  Image,
+  Link,
+  useColorMode,
+} from "@chakra-ui/react";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import { AiFillHome } from "react-icons/ai";
 import { RxAvatar } from "react-icons/rx";
@@ -11,50 +18,51 @@ import userAtom from "../atoms/userAtom";
 import useLogout from "../hooks/useLogout";
 
 const Header = () => {
-	const { colorMode, toggleColorMode } = useColorMode();
-	const user = useRecoilValue(userAtom)
-	const logout = useLogout();
-	const setAuthScreen = useSetRecoilState(authScreenAtom);
+  const { colorMode, toggleColorMode } = useColorMode();
+  const user = useRecoilValue(userAtom);
+  const logout = useLogout();
+  const setAuthScreen = useSetRecoilState(authScreenAtom);
 
-	return (
-		<Flex justifyContent={"space-between"} mt={6} mb='12'>
-			{user && (
-				<Link as={RouterLink} to='/'>
-					<AiFillHome size={24} />
-				</Link>
-			)}
-			{!user && (
-				<Link as={RouterLink} to={"/auth"} 
-				onClick={() => setAuthScreen("login")}
-				>
-					Login
-				</Link>
-			)}
+  return (
+    <Flex pt={6} pb={6} pr={"1rem"} w={"95vw"}>
+      <Image
+        cursor={"pointer"}
+        alt="logo"
+        w={6}
+        src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
+        onClick={toggleColorMode}
+      />
+      {user && (
+        <Link as={RouterLink} to="/">
+          <AiFillHome size={24} />
+        </Link>
+      )}
+      <Spacer />
+      <Flex w="150px" justifyContent={"space-between"}>
+        {!user && (
+          <Link
+            as={RouterLink}
+            to={"/auth"}
+            onClick={() => setAuthScreen("login")}
+          >
+            Login
+          </Link>
+        )}
 
-			<Image
-				cursor={"pointer"}
-				alt='logo'
-				w={6}
-				src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
-				onClick={toggleColorMode}
-			/>
-
-			{user && (
-				<Flex alignItems={"center"} gap={4}>
-					<Link as={RouterLink} to={`/${user.username}`}>
-						<RxAvatar size={24} />
-					</Link>
-					<Link as={RouterLink} to={`/chat`}>
-						<BsFillChatQuoteFill size={20} />
-					</Link>
-					<Button size={"xs"}
-						onClick={logout}
-					>
-						<FiLogOut size={20} />
-					</Button>
-				</Flex>
-			)}
-			{/* {user && (
+        {user && (
+          <Flex alignItems={"center"} gap={4}>
+            <Link as={RouterLink} to={`/${user.username}`}>
+              <RxAvatar size={24} />
+            </Link>
+            <Link as={RouterLink} to={`/chat`}>
+              <BsFillChatQuoteFill size={20} />
+            </Link>
+            <Button size={"xs"} onClick={logout}>
+              <FiLogOut size={20} />
+            </Button>
+          </Flex>
+        )}
+        {/* {user && (
 				<Flex alignItems={"center"} gap={4}>
 					<Link as={RouterLink} to={`/${user.username}`}>
 						<RxAvatar size={24} />
@@ -71,15 +79,18 @@ const Header = () => {
 				</Flex>
 			)} */}
 
-			{!user && (
-				<Link as={RouterLink} to={"/auth"} 
-				onClick={() => setAuthScreen("signup")}
-				>
-					Sign up
-				</Link>
-			)}
-		</Flex>
-	);
+        {!user && (
+          <Link
+            as={RouterLink}
+            to={"/auth"}
+            onClick={() => setAuthScreen("signup")}
+          >
+            Sign up
+          </Link>
+        )}
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Header;
--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -1,17 +1,22 @@
-import { useRecoilValue } from "recoil"
-import authScreenAtom from "../atoms/authAtom"
-import SignupCard from "../components/SignupCard"
+import { useRecoilValue } from "recoil";
+import authScreenAtom from "../atoms/authAtom";
+import SignupCard from "../components/SignupCard";
 import LoginCard from "../components/LoginCard";
+import { Text, Flex, useColorModeValue } from "@chakra-ui/react";
 
 const AuthPage = () => {
+  const bg = useColorModeValue("#d1c8b4", "#839174");
   const authScreenState = useRecoilValue(authScreenAtom);
-  console.log(authScreenState)
+  console.log(authScreenState);
   return (
-    <>
-     {authScreenState==='login'? <LoginCard/> : <SignupCard/>}
-    </>
-  )
-}
+    <Flex gap={"10rem"} w={"98vw"} bg={bg}>
+      <Text fontSize="8xl" pt={"3rem"} pl={"10rem"}>
+        Building Threads Clone
+      </Text>
 
-export default AuthPage
+      {authScreenState === "login" ? <LoginCard /> : <SignupCard />}
+    </Flex>
+  );
+};
 
+export default AuthPage;
